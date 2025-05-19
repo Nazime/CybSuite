b1b3e531ec28f32c48f85c67c707cf5aa3eb4d82
Lakehal Nazime <nazime.lkh@gmail.com>
2025-05-19 17:00:28 +0200

    u

--- tests/test_review/test_windows/test_plugins/test_plugin_bitlocker.py
+++ tests/test_review/test_windows/test_plugins/test_plugin_bitlocker.py
@@ -1,19 +1,25 @@
-from cybsuite.review import pm_reviewer, ReviewManager
+from cybsuite.review import ReviewManager
 
 from .utils import get_data_path
+
 
 def test_plugin_bitlocker(new_cyberdb):
     plugin_name = "bitlocker_encryption"
 
     review_manager = ReviewManager(
         cyberdb=new_cyberdb,
-        name=plugin_name,
-        category="windows",
+        plugins_names=[plugin_name],
     )
 
-    review_manager.review_files({"bitlocker_volumes": get_data_path("bitlocker_volumes.json")})
-    
-    controls = new_cyberdb.request("control", control_definition="windows:bitlocker")
+    review_manager.review_files(
+        {"bitlocker_volumes": get_data_path("bitlocker_volumes.json")}
+    )
+
+    controls = new_cyberdb.request(
+        "control", control_definition__name="windows:bitlocker"
+    )
     assert len(controls) == 1
 
-    pass
+    control = controls[0]
+    assert control.details["mount_point"] == "C:"
+    assert control.status == "ok"
